3a4f767f1c71b2c410a053d2f429a098c6a063ac
baseball435 <jmrapp1@comcast.net>
2013-03-10 18:33:00 -0700

    Finished coding and testing.

--- src/org/omega/autoomega/Boot.java
+++ src/org/omega/autoomega/Boot.java
@@ -11,8 +11,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.logging.Logger;
 
@@ -21,25 +19,26 @@ public class Boot extends JavaPlugin {
     private static final int DEFAULT_MIN = 0, DEFAULT_HOUR = 2;
 
     private static final Logger log = Logger.getLogger("Minecraft");
-    private int lastRestart; //iI minutes
+    private long lastRestart;
     private int timeLimit;
 
     public void onEnable() {
         loadConfig();
-        lastRestart = (int) (System.currentTimeMillis() / (1000 * 60)); //Get the minutes at which the server started
-
+        lastRestart = System.currentTimeMillis(); //Get the minutes at which the server started
         getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            public void run()
-            {
+            public void run() {
                 //Find out if the time is up
-                int minutes = (int) (System.currentTimeMillis() / (1000 * 60));
-                int remaining = minutes - lastRestart;
-                if (remaining <= 5) //Restarting in 5 minutes
-                    getServer().broadcastMessage("Restarting in five minutes!");
-                if (remaining <= 1) //Restarting in 1 minute
-                    getServer().broadcastMessage("Restarting in one minute!");
-                if (remaining >= timeLimit) //If the time passed since the last restart is greater than or equal to the time limit then restart
+                long ms = System.currentTimeMillis();
+                long remaining = timeLimit - (ms - lastRestart);
+                if (remaining <= 0) //If the time passed since the last restart is greater than or equal to the time limit then restart
                     restartServer();
+                else if (remaining <= 10000 && remaining >= 9000)
+                    getServer().broadcastMessage("[Auto Omega] Restarting in 10 seconds!");
+                else if (remaining <= 60000 && remaining >= 59000) //Restarting in 1 minute
+                    getServer().broadcastMessage("[Auto Omega] Restarting in one minute!");
+                else if (remaining <= 300000 && remaining >= 299000) //Restarting in 5 minutes
+                    getServer().broadcastMessage("[Auto Omega] Restarting in five minutes!");
+
             }
         }, 0L, 20L);  //Will go through the run method every second
 
@@ -58,7 +57,11 @@ public class Boot extends JavaPlugin {
     public void restartServer() {
         for (Player player : getServer().getOnlinePlayers()) //Kick everybody online
             player.kickPlayer("Restarting Server!");
-        getServer().shutdown(); //Restart the server
+        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            public void run() {
+                getServer().shutdown();
+            }
+        }, 100L); //Restart the server
     }
 
     public void loadConfig() {
@@ -84,17 +87,18 @@ public class Boot extends JavaPlugin {
 
         int mins, hours;
 
-        String minStr = config[0].trim().substring(8); //Get just the numbers at the end of the minutes
+        String minStr = config[0].trim().substring(9); //Get just the numbers at the end of the minutes
         if (isInteger(minStr)) { //If its a number
             int min = Integer.parseInt(minStr); //Get the number
             mins = min; //Set it to the desired minutes
             if (min > 60) mins = 60; //If it's greater than 60, make it 60
             if (min < 0) mins = 0;   //If it's less than 0, make it 0
-        } else { //If it's not a number
+        } else { //If it's not a number   v
             mins = DEFAULT_MIN; //Set it to the default
+            log.info("[Auto Omega] Error loading the minutes. Switching to default " + DEFAULT_MIN + " minutes.");
         }
 
-        String hourStr = config[1].trim().substring(6); //Get just the numbers at the end of the hours
+        String hourStr = config[1].trim().substring(7); //Get just the numbers at the end of the hours
         if (isInteger(hourStr)) { //If its a number
             int hour = Integer.parseInt(hourStr); //Get the number
             hours = hour; //Set it to the desired hours
@@ -102,10 +106,12 @@ public class Boot extends JavaPlugin {
             if (hour < 0) hours = 0; //If it's less than 0, make it 0
         } else { //If it's not a number
             hours = DEFAULT_HOUR; //Set it to the default
+            log.info("[Auto Omega] Error loading the hours. Switching to default " + DEFAULT_HOUR + " hours.");
         }
 
-        timeLimit += (hours * 60); //Get the number of minutes and add it to the time limit
-        timeLimit += mins; //Add the minutes
+        //Get the number of milliseconds and add it to the time limit
+        timeLimit += (hours * 3600000);
+        timeLimit += (mins * 60000);
     }
 
     public boolean isInteger(String s) {
