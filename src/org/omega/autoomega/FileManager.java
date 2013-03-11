11b6524dfe69a1cc355c44c230b51fc09974b163
baseball435 <jmrapp1@comcast.net>
2013-03-10 17:16:45 -0700

    Finished coding, havent tested.

--- src/org/omega/autoomega/FileManager.java
+++ src/org/omega/autoomega/FileManager.java
@@ -15,25 +15,24 @@ import java.util.List;
 public class FileManager {
 
     public static File loadFile(String path) {
-        File file = new File(path);
-        if (file.exists())
+        File file = new File(path); //Load file
+        if (file.exists())  //If it exists return it
             return file;
         return null;
     }
 
     public static BufferedReader readFile(File file) {
         try {
-            return new BufferedReader(new FileReader(file));
+            return new BufferedReader(new FileReader(file)); //return the buffered reader for the file
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); //Print out errors
         }
         return null;
     }
 
     public static BufferedWriter writeFile(File file) {
         try {
-            FileWriter fw = new FileWriter(file);
-            return new BufferedWriter(fw);
+            return new BufferedWriter(new FileWriter(file)); //Return the buffered writer for the file
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,17 +40,16 @@ public class FileManager {
     }
 
     public static boolean fileExists(String path) {
-        return new File(path).exists();
+        return new File(path).exists(); //Return if it exits (works for both directories and files)
     }
 
     public static boolean createDirectory(String path) {
-        return new File(path).mkdir();
+        return new File(path).mkdir(); //Create a directory
     }
 
     public static boolean createFile(String path) {
         try {
-            File file = new File(path);
-            return file.createNewFile();
+            return new File(path).createNewFile(); //Create a new file
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,13 +58,13 @@ public class FileManager {
 
     public static String[] readLines(BufferedReader br) {
         try {
-            List<String> lines = new ArrayList<String>();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);
+            List<String> lines = new ArrayList<String>(); //Create an arraylist to add the lines to
+            String line = null; //Set a string that will be set to the lines
+            while ((line = br.readLine()) != null) { //If not the end of the file
+                lines.add(line); //Add it to the array
             }
-            br.close();
-            return lines.toArray(new String[lines.size()]);
+            br.close(); //close the stream
+            return lines.toArray(new String[lines.size()]); //create a new array with the size of the arraylist and return it
         } catch (Exception e) {
             e.printStackTrace();
         }
